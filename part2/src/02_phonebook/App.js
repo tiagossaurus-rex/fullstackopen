e31020856b6779c78b4ab5d36058344ad7e4b484
tiago <maildomr.t@gmail.com>
2019-06-09 22:29:13 +0100

    part 2.6

--- part2/src/02_phonebook/App.js
+++ part2/src/02_phonebook/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import axios from 'axios';
 
 
 const Form = (props) => {
@@ -42,15 +43,18 @@ const Persons = ({persons, filter}) => {
 
 
 const App = () => {
-	const [ persons, setPersons] = useState([
-		{ name: 'Arto Hellas', number: '040-123456' },
-		{ name: 'Ada Lovelace', number: '39-44-5323523' },
-		{ name: 'Dan Abramov', number: '12-43-234345' },
-		{ name: 'Mary Poppendieck', number: '39-23-6423122' }
-	])
+	const [ persons, setPersons] = useState([])
 	const [ newName, setNewName ] = useState('')
 	const [ newNumber, setNewNumber ] = useState('')
 	const [ filter, setFilter ] = useState('')
+
+	useState(() => {
+		axios
+        .get('http://localhost:3001/persons')
+        .then(response => {
+            setPersons(response.data)
+        })
+	}, [])
 
 
 	const handleSubmit = (e) => {
