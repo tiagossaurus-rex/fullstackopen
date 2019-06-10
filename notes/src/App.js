fffc5c4b807a826bd8fce4c0f32e154c14c3efd1
tiago <maildomr.t@gmail.com>
2019-06-10 23:25:55 +0100

    notes 2d

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
 import Note from './components/Note'
+
+import noteService from './services/notes'
 
 const App = () => {
     const [notes, setNotes] = useState([]) 
@@ -8,40 +9,61 @@ const App = () => {
     const [showAll, setShowAll] = useState(true)
 
     useEffect(() => {
-        axios
-        .get('http://localhost:3001/notes')
-        .then(response => {
-            setNotes(response.data)
-        })
+        noteService
+            .getAll()
+            .then(initialNotes  => {
+                setNotes(initialNotes )
+            })
     }, [])
 
     const notesToShow = showAll
         ? notes
         : notes.filter(note => note.important)
 
+    const toggleImportanceOf = id => {
+        const note = notes.find(n => n.id === id)
+        const changedNote = {...note, important: !note.important}
+
+        noteService
+            .update(id, changedNote)
+            .then(returnedNote  => {
+                setNotes(notes.map(note => note.id !== id ? note : returnedNote ))
+            })
+            .catch(error => {
+                alert(
+                  `the note '${note.content}' was already deleted from server`
+                )
+                setNotes(notes.filter(n => n.id !== id))
+            })
+    }
+
     const rows = () => notesToShow.map(note =>
         <Note
-        key={note.id}
-        note={note}
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
         />
     )
     
     const handleNoteChange = (event) => {
-        console.log(event.target.value)
         setNewNote(event.target.value)
     }
 
     const addNote = (event) => {
         event.preventDefault()
         const noteObject = {
-        content: newNote,
-        date: new Date().toISOString(),
-        important: Math.random() > 0.5,
-        id: notes.length + 1,
+            content: newNote,
+            date: new Date().toISOString(),
+            important: Math.random() > 0.5,
         }
 
-        setNotes(notes.concat(noteObject))
-        setNewNote('')
+
+        noteService
+            .create(noteObject)
+            .then(initialNotes  => {
+                setNotes(notes.concat(initialNotes ))
+                setNewNote('')
+            })
     }
 
     return (
